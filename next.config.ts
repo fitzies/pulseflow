8bddc175a87e46944d26cdda5e46442e3d71566f
fitzies <olicrypt@gmail.com>
2026-01-19 22:53:39 +0800

    prisma errors should now be fixed

--- next.config.ts
+++ next.config.ts
@@ -2,14 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
-  outputFileTracingIncludes: {
-    "/**": [
-      "./src/generated/prisma/**/*",
-      "./node_modules/.prisma/client/**/*",
-      "./node_modules/@prisma/client/**/*",
-      "./node_modules/@prisma/engines/**/*",
-    ],
-  },
 };
 
 export default nextConfig;
