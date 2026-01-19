fbfe710ca7514378f4fb4f1675bf5c47433ee5e1
fitzies <olicrypt@gmail.com>
2026-01-19 22:45:53 +0800

    2

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+  outputFileTracingIncludes: {
+    "/**": [
+      "./src/generated/prisma/**/*",
+      "./node_modules/.prisma/client/**/*",
+      "./node_modules/@prisma/client/**/*",
+    ],
+  },
 };
 
 export default nextConfig;
