032192bcf577ef39a11ea280a7d031b347ae22d8
fitzies <olicrypt@gmail.com>
2026-01-23 14:46:33 +0800

    changed logo.tsx

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,9 +1,9 @@
-import { Workflow } from "lucide-react";
+import Image from "next/image";
 
 export default function Logo() {
   return (
-    <div className="w-10 h-10 aspect-square rounded-lg flex items-center justify-center bg-#2C2B25">
-      <Workflow className="text-stone-200" />
+    <div className="w-10 h-10 aspect-square rounded-lg flex items-center justify-center bg-#2C2B25 overflow-hidden">
+      <Image src="/logo.png" alt="Logo" width={40} height={40} className="object-contain" />
     </div>
   );
 }
