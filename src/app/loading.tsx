456bdf232da1794f690c66403cd3bb64e083b5a9
fitzies <olicrypt@gmail.com>
2026-01-26 18:24:34 +0800

    made loading h-100

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,7 +1,7 @@
 import { Spinner } from "@/components/ui/spinner"
 
 export default function Loading() {
-  return <div className="w-screen h-[90vh] flex items-center justify-center">
+  return <div className="w-screen h-screen flex items-center justify-center">
     <Spinner />
   </div>
 }
